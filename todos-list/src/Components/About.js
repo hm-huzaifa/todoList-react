0fad7387e756f46e011ec2f245cf81d1b52e92f4
hm-huzaifa <mhuzaifa150@gmial.com>
2022-01-24 20:09:49 +0500

    final

---
+++ todos-list/src/Components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className="container my-10">
+      <h4>This is about Component</h4>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid maxime
+        nisi laudantium animi, vero omnis praesentium inventore veniam, fugit
+        harum numquam, magni quae quibusdam ipsum quisquam maiores laborum minus
+        ab?
+      </p>
+    </div>
+  );
+};
---
+++ todos-list/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Footer = () => {
+  // let footerStyle = {
+  //   position: "relative",
+  //   width: "100%",
+  //   top: "100vh",
+  // };
+  return (
+    <footer className="bg-dark text-light py-2">
+      <p className="text-center">Copyright &copy; TodosList.com</p>
+    </footer>
+  );
+};
